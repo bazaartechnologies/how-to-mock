e68cf9b6c8f8452da9d6869c876c8b32e1054ebe
hamzaahmedkhan <hamza_ahmed_khan@yahoo.com>
2024-05-28 12:37:25 +0500

    task one solution

--- src/test/java/howtomock/one/OrderServiceJvTest.java
+++ src/test/java/howtomock/one/OrderServiceJvTest.java
@@ -2,15 +2,27 @@ package howtomock.one;
 
 
 import howtomock.java.OrderRepositoryJv;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.never;
 
 class OrderServiceJvTest {
-
+    @Mock
     private OrderRepositoryJv orderRepositoryJv;
-
 
     @BeforeEach
     public void setup() {
-        orderRepositoryJv = new OrderRepositoryJv();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void test() {
+        Assertions.assertTrue(true);
+        verify(orderRepositoryJv, never()).getOrderEntityJv();
     }
 }
